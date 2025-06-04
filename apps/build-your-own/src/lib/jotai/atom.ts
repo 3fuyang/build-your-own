777c52ad107714c16b989ef05fddca50351677a0
fwio <624835339@qq.com>
2025-06-04 15:46:47 +0800

    feat(jotai): composable atoms

---
+++ apps/build-your-own/src/lib/jotai/atom.ts
@@ -0,0 +1,112 @@
+/**
+ * Meta description of an atom, which itself does not hold a value.
+ */
+export interface Atom<Value> {
+  read: Read<Value>;
+}
+
+/**
+ * Read atom values, and track the dependencies if needed.
+ *
+ * Note: Getter is not generic, so is the setter below.
+ */
+export type Getter = <Value>(atom: Atom<Value>) => Value;
+
+/**
+ * Write into atoms
+ */
+export type Setter = <Value, Args extends unknown[], Result>(
+  atom: WritableAtom<Value, Args, Result>,
+  ...args: Args
+) => Result;
+
+/**
+ * User provided reader function
+ */
+type Read<Value> = (get: Getter) => Value;
+
+/**
+ * User provided writer function
+ */
+type Write<Args extends unknown[], Result> = (
+  get: Getter,
+  set: Setter,
+  ...args: Args
+) => Result;
+
+export interface WritableAtom<Value, Args extends unknown[], Result>
+  extends Atom<Value> {
+  write: Write<Args, Result>;
+}
+
+type SetStateAction<Value> = Value | ((prev: Value) => Value);
+
+type PrimitiveAtom<Value> = WritableAtom<Value, [SetStateAction<Value>], void>;
+
+type WithInitialValue<Value> = {
+  init: Value;
+};
+
+/** Various overloads of atom creators */
+
+// primitive atom without initial value
+export function atom<Value>(): PrimitiveAtom<Value | undefined> &
+  WithInitialValue<Value | undefined>;
+
+// primitive atom
+export function atom<Value>(
+  init: Value
+): PrimitiveAtom<Value> & WithInitialValue<Value>;
+
+// writable derived atom
+export function atom<Value, Args extends unknown[], Result>(
+  read: Read<Value>,
+  write: Write<Args, Result>
+): WritableAtom<Value, Args, Result>;
+
+// read-only derived atom
+export function atom<Value>(read: Read<Value>): Atom<Value>;
+
+// write-only derived atom
+export function atom<Value, Args extends unknown[], Result>(
+  init: Value,
+  write: Write<Args, Result>
+): WritableAtom<null, Args, Result> & WithInitialValue<Value>;
+
+export function atom<Value, Args extends unknown[], Result>(
+  read?: Value | Read<Value>,
+  write?: Write<Args, Result>
+) {
+  const config = {} as WritableAtom<Value, Args, Result> &
+    WithInitialValue<Value | undefined>;
+
+  if (typeof read === 'function') {
+    config.read = read as Read<Value>;
+  } else {
+    // Primitive atom or write-only derived atom
+    config.init = read;
+    config.read = defaultRead;
+    config.write = write ?? (defaultWrite as unknown as Write<Args, Result>);
+  }
+  if (write) {
+    config.write = write;
+  }
+  return config;
+}
+
+function defaultRead<Value>(this: Atom<Value>, get: Getter) {
+  return get(this);
+}
+
+function defaultWrite<Value>(
+  this: PrimitiveAtom<Value>,
+  get: Getter,
+  set: Setter,
+  arg: SetStateAction<Value>
+) {
+  return set(
+    this,
+    typeof arg === 'function' ? (arg as (prev: Value) => Value)(get(this)) : arg
+  );
+}
+
--- apps/build-your-own/src/lib/jotai/use-atom.ts
+++ apps/build-your-own/src/lib/jotai/use-atom.ts
@@ -1,4 +1,6 @@
-import type { Atom, Getter } from './atom';
+import { useCallback, useEffect, useReducer } from 'react';
+
+import type { Atom, Getter, Setter, WritableAtom } from './atom';
 
 type AnyValue = unknown;
 type AnyAtom = Atom<AnyValue>;
@@ -12,7 +14,11 @@ interface AtomState<Value = AnyValue> {
   value?: Value;
   /** Set of listeners to notify when the atom value changes */
   readonly listeners: Set<() => void>;
-  /** Set of mounted atoms that depends on this atom */
+  /**
+   * Set of mounted atoms that depends on this atom
+   *
+   * > If B depends on A, it means that A is a dependency of B, and B is a dependent on A.
+   */
   readonly dependents: Set<AnyAtom>;
 }
 
@@ -37,7 +43,8 @@ function readAtomState<Value>(atom: Atom<Value>): AtomState<Value> {
     return aState.value as V;
   };
 
-  // Calls user provided read function here
+  // Calls user provided read function here,
+  // calculating the value and assigning it to the atom state
   const value = atom.read(getter);
   atomState.value = value;
 
@@ -56,6 +63,97 @@ function ensureAtomState<Value>(atom: Atom<Value>): AtomState<Value> {
   return atomState;
 }
 
+/**
+ * Write into an atom and notify all its observers
+ */
+function writeAtomState<Value, Args extends unknown[], Result>(
+  atom: WritableAtom<Value, Args, Result>,
+  ...args: Args
+): Result {
+  const getter: Getter = <V>(a: Atom<V>) => readAtomState(a).value as V
+  const setter: Setter = <V, As extends unknown[], R>(a: WritableAtom<V, As, R>, ...args: As) => {
+    const aState = ensureAtomState(a)
+    if (isSelfAtom(atom, a)) {
+      const v = args[0] as V
+      aState.value = v
+      notify(atom)
+
+      return undefined as R
+    }
+    return writeAtomState(a, ...args)
+  }
+
+  return atom.write(getter, setter, ...args)
+}
+
 function isSelfAtom(atom: AnyAtom, a: AnyAtom): boolean {
   return atom === a;
+}
+
+/**
+ * Notify all the dependent atoms, run callbacks for all
+ * the components that are dependent on this atom.
+ *
+ * NOTE: In actual implementation, it's implemented by
+ * `mountDependencies`, maybe.
+ */
+function notify(atom: AnyAtom) {
+  const atomState = ensureAtomState(atom)
+  for (const a of atomState.dependents) {
+    notify(a)
+  }
+  for (const l of atomState.listeners) {
+    l()
+  }
+}
+
+export function useAtom<Value, Args extends unknown[], Result>(atom: Atom<Value> | WritableAtom<Value, Args, Result>) {
+  return [
+    useAtomValue(atom),
+    useSetAtom(atom as WritableAtom<Value, Args, Result>),
+  ] as const
+}
+
+export function useAtomValue<Value>(atom: Atom<Value>): Value {
+  const [[valueFromReducer, atomFromReducer], rerender] =
+    useReducer<readonly [Value, typeof atom], undefined, []>((prev) => {
+      const atomState = readAtomState(atom)
+      const nextValue = atomState.value
+
+      if (Object.is(nextValue, prev[0]) && prev[1] === atom) {
+        return prev
+      }
+
+      return [nextValue as Value, atom]
+    },
+    undefined,
+    () => [readAtomState(atom).value as Value, atom]
+  )
+
+  let value = valueFromReducer
+  if (atomFromReducer !== atom) {
+    rerender()
+    value = readAtomState(atom).value as Value
+  }
+
+  useEffect(() => {
+    // subscribe
+    const atomState = readAtomState(atom)
+    atomState.listeners.add(rerender)
+
+    rerender()
+
+    // unsubscribe
+    return () => { atomState.listeners.delete(rerender) }
+  }, [atom])
+
+  return value
+}
+
+function useSetAtom<Value, Args extends unknown[], Result>(atom: WritableAtom<Value, Args, Result>) {
+  const setAtom = useCallback((...args: Args) => {
+    return writeAtomState(atom, ...args)
+  }, [atom])
+
+  return setAtom
 }
