ce2d5ed57faadd3ab3d16a314d903e11bdd5fb88
fwio <624835339@qq.com>
2025-06-04 18:25:57 +0800

    feat(jotai): added example for derived atom

--- apps/build-your-own/src/app/app.tsx
+++ apps/build-your-own/src/app/app.tsx
@@ -1,16 +1,45 @@
 import { useAtom } from '../lib/jotai/use-atom';
 import { atom } from '../lib/jotai/atom';
 
-const counter = atom(0);
+const countAtom = atom(0);
+const doubleAtom = atom<number>((get) => get(countAtom) * 2);
 
-export function App() {
-  const [count, setCounter] = useAtom(counter);
-  const onClick = () => setCounter((prev) => prev + 1);
+function Counter() {
+  const [count, setCount] = useAtom(countAtom);
+  const inc = () => setCount(count + 1);
 
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={onClick}>Click</button>
+    <div className="flex items-center gap-4">
+      count: {count}{' '}
+      <button
+        className="border text-sm rounded px-2 py-0.5 hover:border-gray-300 focus-visible:border-gray-300 outline-none transition-colors"
+        onClick={inc}
+      >
+        +1
+      </button>
+    </div>
+  );
+}
+
+function DoubleCounter() {
+  const [count] = useAtom(doubleAtom);
+
+  return <div>doubled count(derived): {count}</div>;
+}
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Mini Jotai</h1>
+      <h2 className="text-lg font-semibold mb-2">Derived Atoms</h2>
+      <section className="border border-dashed p-2 mb-2">
+        <h3 className="font-semibold">{'<Counter />'}</h3>
+        <Counter />
+      </section>
+      <section className="border border-dashed p-2">
+        <h3 className="font-semibold">{'<DoubleCounter />'}</h3>
+        <DoubleCounter />
+      </section>
     </div>
   );
 }
